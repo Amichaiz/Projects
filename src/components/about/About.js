e6a82c195bf6a55f53e92fca3ef734744c7c534a
Amichai Berger <amichaiberger14@gmail.com>
2021-04-16 10:56:12 +0300

    g

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ const App = () => {
         <>
         <h2 className='tc'>Amichai Berger</h2>
         <span className='contact'>
-            <p className='aboutp'>Most of the projects in the NavBar where created during my course in developers institute, All of them were created in react. Hope you enjoy.</p>
+            <p className='aboutp'>Most of the projects in the NavBar where created during my course in Developers Institute, All of them were created in React. Hope you enjoy.</p>
         <p className='aboutp'><a href="mailto:amichaiberger14@gmail.com">Send email</a></p>
         <p className='aboutp'><a href="tel:+972543989385">Text me</a></p>
         <p className='aboutp'><a href="https://amichaiz.github.io/" target="_blank" rel='noreferrer'>Bio</a></p>
